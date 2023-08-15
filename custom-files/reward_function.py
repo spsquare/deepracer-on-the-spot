e0a9cfb347780a227331bf0e870c90ce4a9db167
spsquare <spspranav3@gmail.com>
2023-08-15 14:50:54 +0530

    EC2 Training

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,67 +1,9 @@
-import math
-def progress_reward(params):
-    return 1 + params['progress']/50
-def distance_from_center_reward(params):
-    # Read input parameters
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
-
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        return 1e-9  # likely crashed/ close to off track
-
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
-
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
-    # Checking the changes
-    return (1 + float(reward))/2
-
-def speed_reward(params):
-    steering_angle = params['steering_angle']
-    opt_speed = params['speed']*(50/(50+steering_angle))
-    return (4- abs(params['speed'] - opt_speed))/2
-    
-def heading_reward(params):
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
-
-    # Initialize the reward with typical value
-    reward = 1.0
-
-    # Calculate the direction of the center line based on the closest waypoints
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
-
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    # Penalize the reward if the difference is too large
-    DIRECTION_THRESHOLD = 10.0
-    if direction_diff > DIRECTION_THRESHOLD:
-        reward *= 0.6
-    return (1 + float(reward))/2
 def reward_function(params):
     if params['is_offtrack'] or params['is_crashed']:
         return 1e-9
-    return (1+((speed_reward(params)+distance_from_center_reward(params)+heading_reward(params)+progress_reward(params))/6))**6
+    if params['steps'] > 0:
+        reward =(params['progress'])/(params['steps']*100)+(params['speed']**2)
+    else:
+        reward = 1e-9
+
+    return float(reward)
