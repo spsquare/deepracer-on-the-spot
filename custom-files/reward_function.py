efc404f40c76ed0b7efe4a7585b8cbc768554881
spsquare <spspranav3@gmail.com>
2023-08-27 00:01:37 +0530

    v34-ec2

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,54 +1,93 @@
-import math
+import math;
 
 def reward_function(params):
     if params['is_offtrack'] or params['is_crashed']:
-        return 1e-9
-    shorter_turn_reward = 1.1
-    waypoints = params['waypoints']
+        return 1e-9;
+    progress = progress_reward(params);
+    heading = params['heading'];
+    steering_angle = params['steering_angle'];
+    if(heading<0):
+        heading = heading+360;
+    waypoints = params['waypoints'];
     closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
-    # Calculate the direction of the center line based on the closest waypoints
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
-    if len(waypoints) > closest_waypoints[1]+1:
-        next_point_2 = waypoints[closest_waypoints[1]+1]
-    else:
-        next_point_2 = waypoints[0]
-    angle = math.degrees(math.atan2(next_point_2[1]-next_point[1], next_point_2[0]-next_point[0]) - math.atan2(prev_point[1]-next_point[1], prev_point[0]-next_point[0]))
-    angle= abs(angle)
-
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    next_point = waypoints[closest_waypoints[1]];
+    prev_point = waypoints[closest_waypoints[0]];
     track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    # Convert to degree
     track_direction = math.degrees(track_direction)
-
-    # Calculate the difference between the track direction and the heading direction of the car
-    expected_direction= abs(track_direction - heading)
-    direction_diff = abs(track_direction - heading-params['steering_angle'])
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    if expected_direction > 180:
-        expected_direction = 360 - expected_direction
-
-    # Penalize the reward if the difference is too large
-    reward = 1
-    if direction_diff>45:
-        return 1e-9
-    direction_penalty = max(1-abs(expected_direction / 30), 1e-9)
-    if params['steps'] > 0:
-        progress_reward =(params['progress'])/(params['steps']*10)
-        speed_penalty = 2- (abs(params['speed']-4.0*(90/(90+angle)))/4.0)
-        reward *= speed_penalty**4
-        if angle < 20:
-            reward*=speed_penalty**2
-            if params['distance_from_centre']/params['track_width']  <0.1 and angle <10:
-                reward*=0.1
+    if(track_direction<0):
+        track_direction=track_direction+360;
+    all_straight = True;
+    all_increasing = True;
+    all_decreasing = True;
+    index = 5;
+    headings = [];
+    reward = 1.0;
+    max_heading = abs(heading-track_direction);
+    speed_thresh = 2;
+    if(abs(track_direction-heading)>3):
+        reward = reward*0.5;
+    headings.append(track_direction);
+    for i in range(1,5):
+        next_point = waypoints[(closest_waypoints[1]+i)%len(closest_waypoints)]
+        prev_point = waypoints[(closest_waypoints[1]+i-1)%len(closest_waypoints)]
+        track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]);
+        track_direction = math.degrees(track_direction)
+        if(track_direction<0):
+            track_direction=track_direction+360;
+        if(track_direction!=headings[-1]):
+            if(abs(heading-track_direction)>max_heading):
+                max_heading = abs(heading-track_direction);
+                index = i;
+            all_straight = False;
+        if(track_direction>headings[-1]):
+            all_decreasing = False
+        if(track_direction<headings[-1]):
+            all_increasing = False;
+        headings.append(track_direction)
+    if(all_straight):
+        if(params['speed']<speed_thresh):
+            reward = reward*0.2;
         else:
-            progress_reward=progress_reward*2.5
-
+            reward = reward*(1+params['speed']/4)
+        if not params['all_wheels_on_track']:
+            reward = reward * 0.4;    
+        if (abs(steering_angle)>2):
+            reward = reward * 0.3;
     else:
-        return 1e-9
-    if params['is_left_of_center']:
-        reward*=shorter_turn_reward
-    reward=reward*direction_penalty
-    return float(reward+progress_reward)
+        opt_speed = 4;
+        if(index!=5):
+            opt_speed = 72/(18+max_heading)
+        interval = (speed_thresh-opt_speed)/4;
+        if(interval<=0):
+            interval = 0.1;
+        if(params['speed']<opt_speed):
+            reward = reward * 0.1;
+        if(index==1):
+            if(abs(params['speed']-opt_speed)>0.2):
+                reward = reward*0.2;
+            if(abs(headings[1]-headings[0]-params['steering_angle'])>3):
+                reward = reward*0.3;
+            if(all_decreasing and not params['is_left_of_center']):
+                reward = reward * 0.5;
+            if(all_increasing and params['is_left_of_center']):
+                reward = reward * 0.5;
+        elif(index==2):
+            if(abs(params['speed']-opt_speed-0.1)>0.2):
+                reward = reward*0.2;
+        elif(index==3):
+            if(abs(params['speed']-opt_speed-0.3)>0.2):
+                reward = reward*0.2
+        elif(index==4):
+            if(abs(params['speed']-opt_speed-0.5)>0.2):
+                reward = reward*0.2;
+        
+    return float(1+reward)*progress;
+    
+    
+    
+def progress_reward(params):
+    progress = params['progress']
+    steps = params['steps']+1
+    track_length = params['track_length']
+    reward = (progress)/(100*steps)
+    return 1+reward;
