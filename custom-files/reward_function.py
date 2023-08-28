06f80258971f3d16dece8108fd63dc8044d63d07
spsquare <spspranav3@gmail.com>
2023-08-29 00:36:56 +0530

    Enpassant

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,55 +1,39 @@
 import math
 
-def steering_reward(reward, steering, is_left_of_center, speed):
-    if (steering < -25 and is_left_of_center == False) or (steering > 25 and is_left_of_center == True):
-        reward *= 1.2
-    else:
-        reward *= 0.8
-
-    if abs(steering) < 15 and speed < 4:
-        reward *= 1.1
-    else:
-        reward *= 0.9
-
-    if abs(steering) < 1 and speed > 4:
-        reward *= 1.4
-    
-    return reward
-
 def reward_function(params):
-    all_wheels_on_track = params['all_wheels_on_track']         
-    distance_from_center = params['distance_from_center']   
-    progress = params['progress']                           
-    steps = params['steps']                                 
-    speed = params['speed']                                 
-    steering_angle = abs(params['steering_angle'])              
-    track_width = params['track_width']
-    is_left_of_center = params['is_left_of_center']
+    if params['is_offtrack'] or params['is_crashed']:
+        return 1e-9
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
 
-    reward = 1e-3
-    
-    marker_0 = 0.05 * track_width
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    # Calculate the difference between the track direction and the heading direction of the car
+    expected_direction= abs(track_direction - heading)
+    direction_diff = abs(track_direction - heading-params['steering_angle'])
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+    if expected_direction > 180:
+        expected_direction = 360 - expected_direction
 
-    if (all_wheels_on_track and (steps > 0)):
-        reward = ((progress / steps) * 100) + (speed ** 2)
-        if (distance_from_center >= 0.0 and distance_from_center <= marker_0):
-            reward += 7.5
-        elif (distance_from_center <= marker_1):
-            reward += 4.0
-        elif (distance_from_center <= marker_2):
-            reward += 2.5
-        elif (distance_from_center <= marker_3):
-            reward += 0.1
+    # Penalize the reward if the difference is too large
+    reward = 1
+    if direction_diff>45:
+        return 1e-9
+    steering_reward = 100/(1+expected_direction)
+    if params['steps'] > 0:
+        progress_reward =(params['progress'])/(params['steps']*50)
+        speed_penalty = 2- (abs(params['speed']-3.2*(30/(30+abs(params['steering_angle']))))/3.2)
+        reward *= speed_penalty**4
     else:
-        reward -= 0.01
-        
-    reward = steering_reward(reward, steering_angle, is_left_of_center, speed)
-
-    # ABS_STEERING_THRESHOLD = 22
-    # if steering_angle > ABS_STEERING_THRESHOLD:
-    #     reward *= 0.8
-    
-    return float(reward)
+        return 1e-9
+    reward=reward+steering_reward
+    if not (params['is_left_of_center'] and params['distance_from_center'] >= 0.4*params['track_width']):
+        reward=reward * 1.25 if params['steering_angle']>=0 else reward
+    return float(reward+progress_reward)
