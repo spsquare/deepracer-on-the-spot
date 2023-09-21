b1b5a5f5f09b4c76c25a6f8a8e85a56c7f535084
spsquare <spspranav3@gmail.com>
2023-09-22 00:07:04 +0530

    ec2 v1

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -8,21 +8,35 @@ def reward_function(params):
     heading = params['heading']
     # Calculate the direction of the center line based on the closest waypoints
     waypoints_length= len(waypoints)
-    next_point_1 = waypoints[(closest_waypoints[1]-2+waypoints_length)%waypoints_length]
-    next_point_2 = waypoints[(closest_waypoints[1]+0)%waypoints_length]
-    next_point_3 = waypoints[(closest_waypoints[1]+2)%waypoints_length]
-    next_point_4 = waypoints[(closest_waypoints[1]+4)%waypoints_length]
+    next_point_1 = waypoints[closest_waypoints[1]]
+    next_point_2 = waypoints[(closest_waypoints[1]+1)%waypoints_length]
+    next_point_3 = waypoints[(closest_waypoints[1]+2+waypoints_length)%waypoints_length]
+    next_point_4 = waypoints[(closest_waypoints[1]+3+waypoints_length)%waypoints_length]
+    prev_point = waypoints[closest_waypoints[0]]
+
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point_1[1] - prev_point[1], next_point_1[0] - prev_point[0])
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_diff = abs(track_direction - params['heading'])
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+
+    # Penalize the reward if the difference is too large
     angle1 = abs(math.degrees(math.atan2(next_point_2[1]-next_point_1[1],next_point_2[0]-next_point_1[0])))
     angle2 = abs(math.degrees(math.atan2(next_point_4[1]-next_point_3[1],next_point_4[0]-next_point_3[0])))
     reward = 1e-9
     angle = angle2 - angle1
-    steering_reward = 32/(1+abs(params['steering_angle']-angle))
-    if abs(angle) <2:
-        angle=0
+    if abs(angle) <=2:
+        angle = 0
+    angle *=1.5
     if abs(angle) >30:
         optimal_speed=1.5
     else:
         optimal_speed = 12*(1/(3+10*abs(math.sin(abs(angle)))))
+    steering_reward = 32/(1+abs(params['steering_angle']-angle)**2)
     if params['steps'] > 0:
         progress_reward =(params['progress'])/(params['steps']*10)
         speed_penalty = (4 - abs(params['speed']- optimal_speed))
@@ -31,6 +45,12 @@ def reward_function(params):
     else:
         return 1e-9
     reward=reward+steering_reward
-    if optimal_speed < 2.0 and params['is_left_of_center']:
-        reward*=1.5
+    DIRECTION_THRESHOLD = 10.0
+    if direction_diff > DIRECTION_THRESHOLD:
+        reward *= 0.5
+    if not params['all_wheels_on_track']:
+        if params['is_left_of_center'] and params['steering_angle'] >0:
+            reward*=0.1
+        if not params['is_left_of_center'] and params['steering_angle'] <0:
+            reward*=0.1
     return float(reward)
