88ed66199be0851ab5b72679db1c359be08b47ff
subhashkay90787 <subhashkay907871@gmail.com>
2023-10-14 13:39:07 +0530

    test

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -14,9 +14,9 @@ def gen_angle(nxt,prev):
     return deg
 
 def get_abs_speed(diff):
-    if(diff<5):
+    if(diff<6):
         return 4;
-    return max(1.3, 4*math.cos(math.pi*diff/50))
+    return max(1.3, 4*math.cos(math.pi*diff/70))
 
 def progress_reward_func(params):
     progress = params['progress']
@@ -29,77 +29,108 @@ def reward_function(params):
     if params['is_offtrack'] or params['is_crashed']:
         return 1e-9
     waypoints = params['waypoints'];
+    left_of_center = params['is_left_of_center'];
     start = int(params['closest_waypoints'][0]);
     end = int(params['closest_waypoints'][1]);
     cs = params['speed'];
-    len_wp = len(waypoints);
-    
-    while(waypoints[start%len_wp][0]==waypoints[end%len_wp][0] and waypoints[start%len_wp][1]==waypoints[end%len_wp][1]):
-        end+=1;
-        end = end%len_wp;
-    
-    curr = gen_angle(waypoints[end%len_wp],waypoints[start%len_wp])
-    nxt = gen_angle(waypoints[(end+2)%len_wp],waypoints[end%len_wp]);
-    prev = gen_angle(waypoints[(start)%len_wp],waypoints[(start-2+len_wp)%len_wp])
-
-    diff1 = get_diff(curr,prev);
-    diff2 = get_diff(nxt,curr);
-    
-    total = (diff1+diff2)/2;
-    
-    req_steer = round(total)
-    req_speed = round(get_abs_speed(abs(req_steer)),1);
-    
-    direction_diff = abs(curr-params['heading']);
-    steer_diff = abs(params['steering_angle']-req_steer);
-    direction_reward = 100/(1+(direction_diff))  
-    steering_reward = 100/(1+(steer_diff));
-    speed_diff = 10*(abs(cs-req_speed));
-    if(abs(req_steer)>=5 and params['steering_angle']*req_steer<0):
-        return 1e-9;
-    progress_reward = progress_reward_func(params);
-
-    speed_reward = 100/(1+speed_diff);
-    if(abs(req_steer)<5):
-        if(cs>3):
-            speed_reward+=10;
-        if(cs>3.2):
-            speed_reward+=10;
-        if(cs>3.4):
-            speed_reward+=10;
-        if(cs>3.6):
-            speed_reward+=10;
-        if(cs>3.8):
-            speed_reward+=10;
-        if(cs>4):
-            speed_reward+=20;
-    
+    steering = params['steering_angle'];
     track_width = params['track_width'];
     distance_from_center = params['distance_from_center']
-    left_of_center = params['is_left_of_center'];
-    marker_1 = round(min(track_width/2.2,max(track_width/20,(req_steer/30)*(track_width/2))),2);
-    reward = direction_reward+speed_reward+progress_reward
-    if(abs(req_steer)>=5):
-        marker_1 = min(1,(abs(req_steer)+5)/30)*track_width/2;
-        distance_from_center_reward = 0;
-        if(distance_from_center>0.1*marker_1):
-            distance_from_center_reward += 20;
-        if(distance_from_center>0.3*marker_1):
-            distance_from_center_reward += 20;
-        if(distance_from_center>0.5*marker_1):
-            distance_from_center_reward += 20;
-        if(distance_from_center>0.7*marker_1):
-            distance_from_center_reward += 20;
-            
-        if(distance_from_center>marker_1):
-            distance_from_center_reward = 0;
-        if(req_steer>5 and left_of_center):
-            distance_from_center_reward+=30;
-        elif(req_steer<-5 and not left_of_center):
-            distance_from_center_reward+=30;
-        
+    len_wp = len(waypoints);
+    curr = gen_angle(waypoints[end%len_wp],waypoints[(start-1+len_wp)%len_wp])
 
-            
-        reward+=distance_from_center_reward;
-    return reward;
+    angles = []
+    for k in range(end+1,end+4):
+        nxt = waypoints[k%len_wp];
+        prev = waypoints[(k-1+len_wp)%len_wp];
+        if(nxt[1]==prev[1] and nxt[0]==prev[0]):
+            continue;
+        angles.append(gen_angle(nxt,prev));
+    prev_angles = [];
+    for k in range(start-4,start-1):
+        nxt = waypoints[(k+len_wp)%len_wp];
+        prev = waypoints[(k-1+len_wp)%len_wp];
+        if(nxt[1]==prev[1] and nxt[0]==prev[0]):
+            continue;
+        prev_angles.append(gen_angle(nxt,prev));
+    prev_angles.append(curr);
+    total = 0;
+    window = 2;
+    prev_total = 0;
+    for k in range(window,len(angles)):
+        total=total+(get_diff(angles[k],angles[k-window])/k);
+
+    for k in range(window,len(prev_angles)):
+        prev_total=prev_total+(get_diff(prev_angles[k],prev_angles[k-window])/k);
+
+    if(abs(total)>30):
+        if(total>0):
+            total = 30
+        else:
+            total = -30;
+    if(abs(prev_total)>30):
+        if(prev_total>0):
+            prev_total = 30
+        else:
+            prev_total = -30;
+    req_steer = round((total+prev_total)/2)
+    req_speed = round(get_abs_speed(abs(req_steer)),1);
+    reward = 0;
+    speed_diff = abs(cs-req_speed);
+    re_speed = 100/(1+10*speed_diff);
+    re_steer = 100/(1+10*abs(steering-req_steer));
+    if(abs(req_steer)<6):
+        if(cs>3.0):
+            re_speed+=10;
+        if(cs>3.2):
+            re_speed+=10;
+        if(cs>3.4):
+            re_speed+=10;
+        if(cs>3.6):
+            re_speed+=10;
+        if(cs>3.8):
+            re_speed+=10;
+        if(cs>4.0):
+            re_speed+=40;
+
+        re_distance = 0;
+        if(distance_from_center<0.5*track_width):
+            re_distance+=10;
+        if(distance_from_center<0.4*track_width):
+            re_distance+=10;
+        if(distance_from_center<0.3*track_width):
+            re_distance+=10;
+        if(distance_from_center<0.2*track_width):
+            re_distance+=10;
+        if(distance_from_center<0.1*track_width):
+            re_distance+=10;
+        reward+=re_distance;
+        
+        re_heading = 100/(1+abs(curr-params['heading']));
+        reward+=re_heading;
+    else:
+        re_speed = 100/(1+speed_diff);
+        if(req_steer*steering<0):
+            return 1e-9;
+        if(req_steer<0 and not left_of_center):
+            re_steer+=10;
+        elif(req_steer>0 and left_of_center):
+            re_steer+=10;
+        re_distance = 0;
+        marker_1 = track_width/2;
+        if(abs(req_steer)<20):
+            marker_1 = track_width/3;
+        if(abs(req_steer)<15):
+            marker_1 = track_width/4;
+        if(abs(req_steer)<10):
+            marker_1 = track_width/6;
+        if(abs(req_steer)<5):
+            marker_1 = track_width/8;
+        
+        
+        re_distance = 300/(1+10*(abs(distance_from_center-marker_1)/marker_1))
+        reward+=re_distance;
+        
     
+    reward+= re_speed + re_steer + progress_reward_func(params);
+    return reward;
